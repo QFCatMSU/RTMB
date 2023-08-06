ca09a57d935ba23d6683853db394837d4d20b8de
Chris Cahill <christopherfishcahill@gmail.com>
2023-08-05 20:03:52 -0400

    make a plot for matt

--- examples/glmm_spde.r
+++ examples/glmm_spde.r
@@ -1,13 +1,15 @@
 library(RTMB)
 library(tidyverse)
 
-n_site <- 30
-set.seed(12334)
-sim1 <- geoR::grf(n = n_site, cov.pars = c(1, .2)) # var, range
+n_site <- 50
+set.seed(12345)
+spatial_var <- 1
+range <- 0.2
+sim1 <- geoR::grf(n = n_site, cov.pars = c(spatial_var, range)) # var, range
 
 x <- sim1$data
 
-n_per_site <- 30
+n_per_site <- 20
 
 locs <- sim1$coords
 n_site <- nrow(locs)
@@ -40,10 +42,11 @@ my_df <- data.frame(
   y = locs[, 2]
 )
 
-my_df %>%
+p1 <- my_df %>%
   ggplot(aes(x, y, fill = eps)) +
   geom_point(pch = 21) +
-  scale_fill_gradient2()
+  scale_fill_gradient2() +
+  ggtitle("true effects")
 
 parameters <- list(
   beta0 = log(beta0),
@@ -56,7 +59,7 @@ parameters <- list(
 Q_spde <- function(spde, kappa) {
   kappa_pow2 <- kappa * kappa
   kappa_pow4 <- kappa_pow2 * kappa_pow2
-  kappa_pow4 * spde$M0 + 2 * kappa_pow2 * spde$M1 + spde$M2 
+  kappa_pow4 * spde$M0 + 2 * kappa_pow2 * spde$M1 + spde$M2
 }
 
 f <- function(parameters) {
@@ -70,7 +73,7 @@ f <- function(parameters) {
     jnll <- jnll - dpois(y_obs[i], exp(beta0 + eps_s[loc_idx[i]]), TRUE)
   }
   range <- sqrt(8) / exp(log_kappa)
-  sig_o <- 1/sqrt(4*pi * exp(2*log_tau)*exp(2*log_kappa))
+  sig_o <- 1 / sqrt(4 * pi * exp(2 * log_tau) * exp(2 * log_kappa))
   ADREPORT(kappa)
   ADREPORT(range)
   ADREPORT(sig_o)
@@ -80,7 +83,21 @@ f <- function(parameters) {
 obj <- MakeADFun(f, parameters, random = "eps_s")
 opt <- nlminb(obj$par, obj$fn, obj$gr)
 sdrep <- sdreport(obj)
-res <- cbind(sdrep$value, sdrep$value + sdrep$sd %o% c(-1.96, 1.96))
-colnames(res) <- c("MLE", "lower95", "upper95")
+truth <- c(sqrt(8)/range, range, sqrt(spatial_var)) 
+res <- cbind(sdrep$value, sdrep$value + sdrep$sd %o% c(-1.96, 1.96), truth)
+colnames(res) <- c("MLE", "lower95", "upper95", "truth")
 print(res)
 
+my_df <- data.frame(
+  eps = sdrep$par.random[1:n_site],
+  x = locs[, 1],
+  y = locs[, 2]
+)
+
+p2 <- my_df %>%
+  ggplot(aes(x, y, fill = eps)) +
+  geom_point(pch = 21) +
+  scale_fill_gradient2() +
+  ggtitle("estimated random effects")
+
+cowplot::plot_grid(p1, p2)
